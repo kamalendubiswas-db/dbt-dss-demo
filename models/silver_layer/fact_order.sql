f681f7a1914e2c893894bbc0978d6af98f63722b
Kamalendu Biswas <kamalendu.biswas@databricks.com>
2023-08-26 23:04:16 +0200

    fixed fact_order_line

--- models/silver_layer/fact_order.sql
+++ models/silver_layer/fact_order.sql
@@ -15,7 +15,7 @@ SELECT
   CURRENT_DATE        AS  dwh_inserted_at,
   CURRENT_DATE        AS  dwh_upadted_at
 FROM
-  {{ ref('dim_order') }} ord
+  {{ this.schema }}_{{ ref('dim_order') }} ord
   LEFT JOIN   {{ this.schema }}_{{ ref('dim_customer') }}  cust ON cust.customer_id = ord.customer_id
   LEFT JOIN   {{ this.schema }}_{{ ref('dim_nation') }}  nat    ON cust.nation_id   = nat.nation_id   
   INNER JOIN  {{ this.schema }}_{{ ref('dim_date') }}  dt       ON dt.date_full     = ord.order_date
