4e3525e92b27ae3c9e43a680f0d9619a9689e5e3
Gustav <gustavbyberg91@gmail.com>
2023-11-28 10:35:40 +0100

    Test repair run

--- models/silver_layer/dim_region.sql
+++ models/silver_layer/dim_region.sql
@@ -8,7 +8,7 @@
 SELECT
   MD5(region_name || '-' || region_id)  AS region_key,
   region_id                             AS region_id,
-  region_name                           AS region_name,
+  region_names                           AS region_name,
   CURRENT_DATE                          AS dwh_inserted_at,
   CURRENT_DATE                          AS dwh_updated_at
 FROM
